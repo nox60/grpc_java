90d4f08fd2ed337f86cff82e8e976c29b910611f
Nox.Liu <nox60@hotmail.com>
2021-04-28 02:50:25 +0800

    First commit

--- src/main/java/com/grpcserver/server/GrpcServer.java
+++ src/main/java/com/grpcserver/server/GrpcServer.java
@@ -1,7 +1,59 @@
 package com.grpcserver.server;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
 public class GrpcServer {
-    public static void main(String args[]){
-        System.out.println("----");
+
+    private int port = 50051;
+    private Server server;
+
+    private void start() throws IOException {
+        server = ServerBuilder.forPort(port)
+                .addService(new MessageImpl())
+                .build()
+                .start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+
+            @Override
+            public void run() {
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                GrpcServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    // block 一直到退出程序
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        final GrpcServer server = new GrpcServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    // 实现 定义一个实现服务接口的类
+    private class MessageImpl extends MsgServiceGrpc.MsgServiceImplBase {
+        @Override
+        public void sendMsg(MsgRequest req, StreamObserver<MsgResponse> responseObserver) {
+            MsgResponse reply = MsgResponse.newBuilder().setMessage(("Hello From Java " + req.getUsername())).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
     }
 }
