0182f0c41bd06f1f63aa0c5fee340dca4b352b7a
Liu Li <nox60@hotmail.com>
2021-05-02 01:48:43 +0800

    Update GrpcServer.java

--- src/main/java/com/grpcserver/server/GrpcServer.java
+++ src/main/java/com/grpcserver/server/GrpcServer.java
@@ -81,25 +81,4 @@ public class GrpcServer {
     private int port = 50051;
     private Server server;
 
-    private void start() throws IOException {
-        server = ServerBuilder.forPort(port)
-                .addService(new MessageImpl())
-                .build()
-                .start();
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-
-            @Override
-            public void run() {
-                System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                GrpcServer.this.stop();
-                System.err.println("*** server shut down");
-            }
-        });
-    }
-
-    private void stop() {
-        if (server != null) {
-            server.shutdown();
-        }
-    }
 }
