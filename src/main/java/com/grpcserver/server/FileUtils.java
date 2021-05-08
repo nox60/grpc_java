58b1307c6a380302fbff0b5feb275326904779de
LiuLi <nox60@hotmail.com>
2021-05-08 11:41:47 +0800

    Update files

--- src/main/java/com/grpcserver/server/FileUtils.java
+++ src/main/java/com/grpcserver/server/FileUtils.java
@@ -1,6 +1,9 @@
 package com.grpcserver.server;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,13 +12,54 @@ public class FileUtils {
 
     }
 
+    public static String readFileContent(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        StringBuffer sbf = new StringBuffer();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempStr;
+            while ((tempStr = reader.readLine()) != null) {
+                sbf.append(tempStr);
+            }
+            reader.close();
+            return sbf.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return sbf.toString();
+    }
+
     public static String getFiles(){
         StringBuffer stringBuffer = new StringBuffer();
         List<String> files = getFilesByPathAndSuffix();
         if( files!=null && files.size()>0){
+
             for( int i =0;i<files.size();i++){
-                stringBuffer.append(files.get(i)).append("|~|!|");
+                stringBuffer.append("<tr>");
+                stringBuffer.append("<td>");
+                stringBuffer.append(files.get(i));
+                stringBuffer.append("</td>");
+                stringBuffer.append("<td>");
+
+                // 读取文件内容
+                stringBuffer.append(readFileContent(files.get(i)));
+
+                stringBuffer.append("</td>");
+                stringBuffer.append("<td>");
+                stringBuffer.append("选择");
+                stringBuffer.append("</td>");
+                stringBuffer.append("</tr>");
             }
+
         }
         return stringBuffer.toString();
     }
